148a03eb3100919267a2abaa10be97db69e3f892
Daniel Rużewicz <d.ruzew@gmail.com>
2017-12-15 18:17:25 +0100

    napisany progran dziala

--- cwiczenie1.js
+++ cwiczenie1.js
@@ -1,1 +1,13 @@
 var slowa = ["sos", "test", "zaraz", "dwa", "oko"];
+function sprawdzOdwracalnosc(tablicaSlow) {
+    tablicaSlow.forEach((element) => {
+        var check = element.split("");
+        console.log(check)
+        if (check.reverse().join("") ==element) {
+            console.log(element, 'jest odwracalny');
+        } else {
+            console.log(element, ' jest nieodwracalny');
+        }
+    });
+}
+sprawdzOdwracalnosc(slowa);
