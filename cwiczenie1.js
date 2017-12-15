e3e3f9e365f9c19970c6934a91996bbf7bc5d8c1
Daniel Rużewicz <d.ruzew@gmail.com>
2017-12-15 18:21:10 +0100

    srednik

--- cwiczenie1.js
+++ cwiczenie1.js
@@ -2,7 +2,7 @@ var slowa = ["sos", "test", "zaraz", "dwa", "oko"];
 function sprawdzOdwracalnosc(tablicaSlow) {
     tablicaSlow.forEach((element) => {
         var check = element.split("");
-        console.log(check)
+        console.log(check);
         if (check.reverse().join("") ==element) {
             console.log(element, 'jest odwracalny');
         } else {
